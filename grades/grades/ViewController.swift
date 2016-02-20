4e6632cbce8da76472d3ebdccd6f4435d44e56ef
Felipe <palaxis08@gmail.com>
2016-02-19 19:10:26 -0500

    add project

---
+++ grades/grades/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  grades
+//
+//  Created by centro docente de computos on 2/19/16.
+//  Copyright © 2016 UdeM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
